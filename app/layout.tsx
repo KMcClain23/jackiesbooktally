9a50b7ae1cc10b27b65b4ad87f69533498b63f65
snoopyods <snoopyods@gmail.com>
2024-07-02 22:18:03 +0000

    Reduced gaps between paragraph and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-gradient-to-tr overflow-x-hidden min-w-screen from-zinc-950 via-stone-900 to-neutral-950 flex min-h-screen flex-col items-center justify-between">
         <NavBar />
-        <main className="p-4 pt-[64px] gap-6 w-full lg:w-[55%]">
+        <main className="p-4 gap-6 w-full lg:w-[55%]">
           {children}
         </main>
       </body>
